c38497a8ab0c352c277432db8425d3c5de988093
nihad92 <nihad92@gmail.com>
2016-05-05 02:29:57 +0200

    New version of library included

--- app/src/main/java/com/revtwo/revtwo/MainActivity.java
+++ app/src/main/java/com/revtwo/revtwo/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends RActivity {
         revTwo.r2CustomizeIncomingCallBackgroundColor("#CDCDCD");
         revTwo.r2CustomizeIncomingCallTextColor("#EEEEEE");
         revTwo.r2CustomizeIncomingCallImage(R.drawable.rev_two_splash_screen);*/
-        revTwo.r2RegisterActivityForScreenshot(this);
+    revTwo.r2RegisterActivityForScreenshot(this);
     showDemoListFragment();
 
 }
